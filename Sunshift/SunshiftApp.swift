0393c6f18430fb9c0436d2769698dcde58c6f86f
alexatanassov <81797085+alexatanassov@users.noreply.github.com>
2026-06-22 09:59:43 -0700

    Initial Commit

---
+++ Sunshift/SunshiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunshiftApp.swift
+//  Sunshift
+//
+//  Created by Alex Atanassov on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SunshiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SunshiftTests/SunshiftTests.swift
@@ -0,0 +1,19 @@
+//
+//  SunshiftTests.swift
+//  SunshiftTests
+//
+//  Created by Alex Atanassov on 6/22/26.
+//
+
+import Testing
+@testable import Sunshift
+
+struct SunshiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
